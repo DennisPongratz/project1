22efbe4d669421c658590b185b0f383f8d34b570
DennisPongratz <dennis.pongratz@gmx.de>
2018-09-24 08:30:24 -0600

    first commit

---
+++ makefile
@@ -0,0 +1,2 @@
+program1: program1.c
+	gcc -o program1 -Wall program1.c -lm
---
+++ program1.c
@@ -0,0 +1,47 @@
+/* Ryan and Dennis Pongratz
+ * project1, CSCI112
+ * 21/09/2018
+ *
+ * purpose:
+ */
+
+#include <stdio.h>
+#include <math.h>
+
+// declare functions
+
+void getData(double *princ, double *annInt, double *nop);
+
+
+int main(int argc, char** argv) {
+    
+    // declare variables 
+
+    double princ, annInt, nop;
+
+    // get data
+
+    getData(&princ, &annInt, &nop); 
+
+    // do the calculation
+
+    
+
+    // give the data back
+   
+    return(0);
+}
+
+void getData(double *princ, double *annInt, double *nop) {
+    
+    printf("please type in the initial principal: ");
+    scanf("%lf", princ);
+
+    printf("please type in the annual interest: ");
+    scanf("%lf", annInt);
+
+    printf("please type in the number of payments: ");
+    scanf("%lf", nop);
+
+}
+
