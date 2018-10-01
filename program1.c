203340b185e2f9ef4f21416c0e5c95d332c690fb
Ryan Cummings <rcummings98@gmail.com>
2018-10-01 11:33:54 -0600

    with calculate method

--- program1.c
+++ program1.c
@@ -10,21 +10,22 @@
 
 // declare functions
 
-void getData(double *princ, double *annInt, double *nop);
-
+void getData(double *principal, double *annual_interest, double *nop);
+double calculate_payment(double, double, double);
 
 int main(int argc, char** argv) {
-    
+    double payment;
     // declare variables 
 
-    double princ, annInt, nop;
+    double principal, annual_interest, nop;
 
     // get data
 
-    getData(&princ, &annInt, &nop); 
+    getData(&principal, &annual_interest, &nop); 
 
     // do the calculation
-
+    payment = calculate_payment(principal, annual_interest, nop);
+    printf("%lf\n", payment);
     
 
     // give the data back
@@ -32,16 +33,25 @@ int main(int argc, char** argv) {
     return(0);
 }
 
-void getData(double *princ, double *annInt, double *nop) {
+void getData(double *principal, double *annual_interest, double *nop) {
     
     printf("please type in the initial principal: ");
-    scanf("%lf", princ);
+    scanf("%lf", principal);
 
     printf("please type in the annual interest: ");
-    scanf("%lf", annInt);
+    scanf("%lf", annual_interest);
 
     printf("please type in the number of payments: ");
     scanf("%lf", nop);
+
+}
+/* calculates payments */
+double calculate_payment(double principal, double annual_interest, double nop){
+    double payment;
+    double monthly_interest = annual_interest / 12;
+
+    payment = ((monthly_interest * principal) / (1 - pow((1 + monthly_interest), -nop)));
+    return payment;
 
 }
 
